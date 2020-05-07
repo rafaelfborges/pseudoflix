e11c54a68a3c818a00241b29beaa8f8f1f5dfe6c
Rafael Borges <rafaelfborges@gmail.com>
2020-05-07 20:56:07 -0300

    Fix AutenticarUsuario.php

--- src/ListarFavoritos.php
+++ src/ListarFavoritos.php
@@ -7,8 +7,10 @@
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       if(isset($_GET['id'])) {
         $id = $_GET['id'];
-        $sql = "SELECT * FROM favoritos INNER JOIN filmes_series ON favoritos.movie_id = filmes_series.id
-                WHERE favoritos.user_id = '$id'";
+        $sql = "SELECT favoritos.user_id, favoritos.movie_id, filmes_series.titulo, filmes_series.descricao, 
+                        filmes_series.url_poster, filmes_series.url_imdb, filmes_series.url_youtube, filmes_series.genero, 
+                        filmes_series.data_lancamento, filmes_series.tipo FROM favoritos INNER JOIN filmes_series ON 
+                            favoritos.movie_id = filmes_series.id WHERE favoritos.user_id = '$id'";
       }
       $stmt = $pdo->prepare($sql);
       $stmt->execute();

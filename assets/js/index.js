e37918a65cea797f479b31fa40e3af7e6b40931f
Rafael Borges <rafaelfborges@gmail.com>
2020-05-12 16:10:59 -0300

    Fix braces detalhesFilmesSeries function

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,8 @@ function verificaSessao() {
     $("#usuarioMenu").css("visibility", "visible");
     $("#usuarioNotificacao").css("visibility", "visible");
     $("#usuarioPesquisa").css("visibility", "visible");
+    $("#usuarioFavoritos").css("visibility", "visible");
+    $("#usuarioConteudosRecentes").css("visibility", "visible");
     if(verificarPermissaoUsuario() === "admin"){
       $("#acessoAdmin").css("display", "block");
     }
@@ -62,10 +64,12 @@ function detalhesFilmesSeries(id) {
       $("#detalhesConteudo").on('hidden.bs.modal', function (e) {
         $("#conteudoYoutube").attr("src", $("#conteudoYoutube").attr("src"));
       });
-      
+
       $("#conteudoVideo").empty().append(
         `<div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" id="conteudoYoutube" width="100%" height="315" src="${response[0].url_youtube.replace("watch?v=", "embed/")}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe class="embed-responsive-item" id="conteudoYoutube" width="100%" height="315" 
+            src="${response[0].url_youtube.replace("watch?v=", "embed/")}" frameborder="0" 
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         `
       )
@@ -76,8 +80,14 @@ function detalhesFilmesSeries(id) {
          <p class="card-text mb-0"><b>Gênero:</b> ${response[0].genero}</p>
          <p class="card-text mb-0"><b>Data Lançamento:</b> ${response[0].data_lancamento}</p>
          <p class="card-text mb-0"><b>Tipo:</b> ${response[0].tipo}</p>
-         <p class="card-text mb-0"><b>IMDb:</b> <a href="${response[0].url_imdb}" target="_blank">Acessar detalhes do filme</a> </p>
-         <p class="card-text mb-0"><b>Adicionar Favorito: </b><i class="fas fa-star" id="favorito"></i> </p>
+         <p class="card-text mb-0">
+            <b>IMDb:</b> 
+            <a href="${response[0].url_imdb}" target="_blank">Acessar detalhes do filme</a> 
+         </p>
+         <p class="card-text mb-0">
+            <b>Adicionar Favorito: </b>
+            <i class="fas fa-star" id="favorito"></i> 
+         </p>
         `
       )
     },

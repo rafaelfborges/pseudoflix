54ed953eb544c87b13486066c34932e131597305
Rafael Borges <rafaelfborges@gmail.com>
2020-05-29 02:31:45 -0300

    Create User Module

--- src/Usuario.php
+++ src/Usuario.php
@@ -1,18 +1,40 @@
 <?php
+require 'ConexaoBD.php';
+require 'PHPMailer/PHPMailerAutoload.php';
 
-switch ($_GET['acao']) {
-  case "cadastrarFilme":
-    cadastrarFilme();
-    break;
-    
-  case "listarFilme":
-    echo "listar filmes...";
-    break;
-    
-  default:
-    echo "Opção inválida!";
+date_default_timezone_set('America/Sao_Paulo');
+$pdo = Conexao::getInstance();
+
+/**
+ * ### Módulo de Usuário ###
+ * De acordo com ação passado por parâmetro, executa o método correspondente. 
+ */
+if(!empty($_GET)){
+  switch ($_GET['acao']) {
+    case "cadastrar":
+      cadastrarUsuario();
+      break;
+
+    case "listar":
+      echo "listar filmes...";
+      break;
+
+    case "atualizar":
+      echo "atualizar filmes...";
+      break;
+
+    case "deletar":
+      echo "deletar filmes...";
+      break;
+
+    default:
+      echo "Opção inválida!";
+  }
+} else {
+  echo "Nenhum dado foi enviado por parâmetro, verifique sua requisição!";
+  http_response_code(400);
 }
-
-function cadastrarFilme(){
+  
+function cadastrarUsuario(){
   echo "Cadastrando...";
 }

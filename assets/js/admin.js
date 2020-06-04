ef9c32ed8150ee4b8693ab12b46619bd29f95a1e
Rafael Borges <rafaelfborges@gmail.com>
2020-06-04 10:24:52 -0300

    Fix fetch all data in dashboard admin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
   }
   
   verificaSessao();
-  listarConteudo(6);
+  listarTudo();
   cadastrarFilmeSerie();
 });
 
@@ -18,6 +18,7 @@ function verificaSessao() {
 
 function cadastrarFilmeSerie() {
   $("#formCadastroAdmin").submit(function () {
+    event.preventDefault();
     let dados = $(this).serializeArray();
     $.ajax({
       url: "../src/Conteudo.php?acao=cadastrar",
@@ -26,6 +27,7 @@ function cadastrarFilmeSerie() {
       type: "POST",
       dataType: 'JSON',
       success: (response) => {
+        this.reset();
         swal("Deu certo!", "Título cadastrado com sucesso. Verifique em pesquisar.", "success");
       },
       error: (request) => {
@@ -37,16 +39,14 @@ function cadastrarFilmeSerie() {
         }
       }
     })
-    event.preventDefault();
   });
 }
 
-function listarConteudo($limiteConteudo) {
+function listarTudo() {
   $.ajax({
-    url: "../src/Conteudo.php?acao=listar",
+    url: "../src/Conteudo.php?acao=listarTudo",
     cache: false,
     type: "GET",
-    data: { limite: $limiteConteudo },
     dataType: 'JSON',
     success: (response) => {
       const { message } = response;
@@ -59,13 +59,6 @@ function listarConteudo($limiteConteudo) {
             <td>${item.data_lancamento}</td>
             <td>${item.tipo}</td>
             <td>${item.usuario}</td>
-            <td>
-                <a href="#modalDetalhes" data-toggle="modal" data-target='#modalDetalhes' data-id='${item.movie_id}'>
-                    <i class="fas fa-eye"></i>
-                </a> -  
-                <a href="#modalEditar"><i class="fas fa-edit"></i></a> -  
-                <a href="#excluir"><i class="fas fa-trash"></i></a>
-            </td>
           </tr>`
         )
       });
@@ -73,42 +66,5 @@ function listarConteudo($limiteConteudo) {
     error: (request) => {
       console.log(request);
     }
-  });
-}
-
-function visualizarFilmeSerie() {
-  $('#modalDetalhes').on('show.bs.modal', (event) => {
-    let button = $(event.relatedTarget)
-    let id = button.data('id')
-    $.ajax({
-      url: '../src/Conteudo.php?acao=listar',
-      type: 'GET',
-      data: 'id=' + id,
-      dataType: 'JSON',
-      success: function (response) {
-        response.map((item) => {
-          $("#detalhes").empty().append(
-            `<div class="card mb-3" style="max-width: 540px;">
-              <div class="row no-gutters">
-                <div class="col-md-4">
-                  <a href="${item.url_imdb}" target="_blank">
-                    <img src="${item.url_poster}" class="card-img" alt="${item.titulo}">
-                  </a>
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">${item.titulo}</h5>
-                    <p class="card-text mb-0"><b>Descrição:</b> ${item.descricao}</p>
-                    <p class="card-text mb-0"><b>Gênero:</b> ${item.genero}</p>
-                    <p class="card-text mb-0"><b>Data Lançamento:</b> ${item.data_lancamento}</p>
-                    <p class="card-text mb-0"><b>Tipo:</b> ${item.tipo}</p>
-                  </div>
-                </div>
-              </div>
-            </div>`
-          ); 
-        });
-      }
-    });
   });
 }
